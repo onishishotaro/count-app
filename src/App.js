4a2c609a73ab0818247e28379c59ad5a0ce90ea2
onishishotaro <shoukiti.chan@gmail.com>
2021-04-29 20:29:01 +0900

    2回目のコミット

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [num, setNum] = useState(0);
+  const onCountUp = () => {
+    setNum(num + 1);
+  };
+  const onCountDown = () => {
+    setNum(num - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>カウントアプリ</h1>
+      <p>{num}</p>
+      <button onClick={onCountUp}>+1</button>
+      <button onClick={onCountDown}>-1</button>
+    </>
   );
-}
+};
 
 export default App;
